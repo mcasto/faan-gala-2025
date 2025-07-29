508b295d30816ae5cdfb8f26134179e4cca87956
Mike Casto <castoware@gmail.com>
2025-07-29 16:45:46 -0500

    Spanish translation for auction-palooza

---
+++ resources/lang/es/auction-palooza.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'leftPanel' => file_get_contents(__DIR__ . '/html-blocks/auction-palooza/left-panel.html'),
+    'rightPanel' => file_get_contents(__DIR__ . '/html-blocks/auction-palooza/right-panel.html'),
+    'bottomPanel' => file_get_contents(__DIR__ . '/html-blocks/auction-palooza/bottom-panel.html'),
+];
