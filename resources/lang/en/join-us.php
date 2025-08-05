102350d9526fc3d15b1f480a3c890db118e42263
Mike Casto <castoware@gmail.com>
2025-08-05 09:30:19 -0500

    Added paws on red carpet to right panel and removed upper right block from join-us page.

--- resources/lang/en/join-us.php
+++ resources/lang/en/join-us.php
@@ -1,5 +1,4 @@
 <?php
 return [
-    'upperRight' => file_get_contents(__DIR__ . '/html-blocks/join-us/upper-right.html'),
     'rightPanel' => file_get_contents(__DIR__ . '/html-blocks/join-us/right-panel.html'),
 ];
--- resources/views/join-us.blade.php
+++ resources/views/join-us.blade.php
@@ -8,12 +8,6 @@
         <div class="w-full md:w-1/2 relative left-panel" style="background-image: url('storage/images/join-us-bg.jpeg');">
             <div class="absolute inset-0 "></div>
             <div class="relative z-10 flex flex-col justify-between p-8 text-white min-h-[60vh] md:min-h-screen h-full">
-                <div class="join-us-upper-right self-end bg-blue-300/60 p-4 circle-bg mb-4">
-                    {!! $upperRight !!}
-                </div>
-                {{-- <div class="join-us-lower-left circle-bg  bg-blue-800/60">
-                    {!! $lowerLeft !!}
-                </div> --}}
             </div>
         </div>
 
