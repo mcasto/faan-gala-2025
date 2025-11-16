116351afc97e17d90140cefb506705964ac48777
Mike Casto <castoware@gmail.com>
2025-11-16 12:47:42 -0500

    Updated guest info page

--- resources/lang/en/guest-information.php
+++ resources/lang/en/guest-information.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'image' => '/storage/images/guest-information.jpg',
+    'image' => '/storage/images/guest-info-image.jpg',
     'contents' => file_get_contents(__DIR__ . '/html-blocks/guest-information/contents.html')
 ];
